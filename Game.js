744fc42ec67926488fb2da03e05570cfbf2061c1
sayantani-sen <74294422+sayantani-sen@users.noreply.github.com>
2020-12-15 15:38:37 +0530

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,61 @@
+class Game {
+  constructor(){}
+  
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+   
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  start(){
+    if(gameState === 0){
+      player = new Player();
+      player.getCount();
+      form = new Form()
+      form.display();
+    }
+  }
+  
+  play(){
+    form.hide();
+    textSize(30);
+    text("Game Start", 120,100);
+    Player.getPlayerInfo();
+    
+    if(allPlayers !== undefined){
+      var display_position = 130;
+      
+      if(plr === "player" + player.index){
+        fill("red");
+      }
+      else{
+        fill("black");
+      }
+      display_position += 20;
+      textSize(15);
+      text(allPlayers[plr].name + ":" + allPlayer[plr], distance, 120, display_posittion)
+    }
+    }
+  
+    if(keyIsDown(UP_ARROW) && player.index !== null){
+      player.distance += 50;
+      player.update();
+    }
+  
+  }
+  
+  
+}
+
+
+
+
+
